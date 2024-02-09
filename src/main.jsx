d72e906797ec9958979f23a7d2f6361e51bd9381
Vinnie Lopez <vinnielo01@gmail.com>
2024-02-09 10:23:34 -0800

    updated json

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
-import Home from './pages/Home.jsx'
-import ErrorPage from './pages/ErrorPage.jsx'
-import Contact from './pages/Contact.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import Contact from "./pages/Contact.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/srforlandotest/',
+    path: "/lindsaysrforlandotest/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       {
         index: true,
-        element: <Home />
-      }, 
+        element: <Home />,
+      },
       {
-        path: '/srforlandotest/contact', 
-        element: <Contact />
-      }
-    ]
-  }
-])
+        path: "/srforlandotest/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
